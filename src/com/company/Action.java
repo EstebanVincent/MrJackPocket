10eabf735712662bd6a7c9a9db7d6585f7657fb5
Anh Phong VIRASSAMY <williamphong77@gmail.com>
2020-11-13 16:32:16 +0100

    ajout attribut binaire recto verso et ajout des methodes vides

--- src/com/company/Action.java
+++ src/com/company/Action.java
@@ -5,4 +5,41 @@ public class Action {
     public String jeton2;
     public String jeton3;
     public String jeton4;
+    public int face; // recto et verso
+
+    public Action(String jeton1, String jeton2, String jeton3, String jeton4, int face) {
+        this.jeton1 = jeton1;
+        this.jeton2 = jeton2;
+        this.jeton3 = jeton3;
+        this.jeton4 = jeton4;
+        this.face = face;
+    }
+
+    public void Alibi(){
+
+    }
+
+    public void Exchange(){
+
+    }
+
+    public void Rotation(){
+
+    }
+
+    public void Joker(){
+
+    }
+
+    public void Holmes(){
+
+    }
+
+    public void Watson(){
+
+    }
+
+    public void Dogs(){
+
+    }
 }
